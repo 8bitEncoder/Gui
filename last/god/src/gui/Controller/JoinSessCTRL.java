4d3a18a7bbad900625e62f97ae27aca9ae7e2a69
8bitEncoder <fahmipro4646@gmail.com>
2026-01-05 19:52:36 +0800

    third push on 7:51 1/5/2026
    perfected all the page from the sidebar using navigator n making sure all the fxml is same size n loaded equally

--- last/god/src/gui/Controller/JoinSessCTRL.java
+++ last/god/src/gui/Controller/JoinSessCTRL.java
@@ -2,4 +2,5 @@ package gui.Controller;
 
 public class JoinSessCTRL {
     
+    public void refreshSessions(){System.out.println("refreshSessions");}
 }
---
+++ last/god/src/gui/Controller/LogOutCTRL.java
@@ -0,0 +1,12 @@
+package gui.Controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class LogOutCTRL {
+    @FXML Button noBtn;
+    @FXML Button yesBtn;
+
+    public void yes(){System.out.println("yes");}
+    public void no(){System.out.println("no");}
+}
